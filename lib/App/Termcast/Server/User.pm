3b985ff1cd2145667358eadbf9f4224d7016d0d6
Jason May <jason.a.may@gmail.com>
2011-06-12 21:17:04 -0400

    cleanup

--- lib/App/Termcast/Server/User.pm
+++ lib/App/Termcast/Server/User.pm
@@ -6,35 +6,5 @@ use namespace::autoclean;
 
 with qw(KiokuX::User);
 
-=head1 NAME
-
-App::Termcast::User - termcast user class
-
-=head1 SYNOPSIS
-
-TODO
-
-=head1 DESCRIPTION
-
-TODO
-
-=cut
-
-
-__PACKAGE__->meta->make_immutable;
-
 1;
-
-__END__
-
-=head1 METHODS
-
-
-=head1 AUTHOR
-
-Jason May C<< <jason.a.may@gmail.com> >>
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and::or modify it under the same terms as Perl itself.
 
